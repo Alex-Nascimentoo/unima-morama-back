2dd5a7d17e9bee9b5af538fbcb215235dfc7de5b
Guilherme D. Possas <possasg@gmail.com>
2024-05-07 16:09:42 -0300

    remove: update client dto import

--- src/client/client.controller.ts
+++ src/client/client.controller.ts
@@ -1,7 +1,6 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete, HttpCode, HttpStatus } from '@nestjs/common';
 import { ClientService } from './client.service';
 import { CreateClientDto } from './dto/create-client.dto';
-import { UpdateClientDto } from './dto/update-client.dto';
 
 @Controller('/client')
 export class ClientController 
