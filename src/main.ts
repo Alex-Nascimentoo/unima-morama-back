69b51e7d7ed202fdffbb4c73e04500aacb68f4a4
Guilherme D. Possas <possasg@gmail.com>
2024-05-07 11:09:58 -0300

    feat: add validation pipe to use as global pipes and add validation options

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const PORT = 3000;
   const DOMAIN = 'localhost';
+  app.useGlobalPipes( new ValidationPipe(
+    {
+      transform:true,
+      whitelist: true,
+      forbidNonWhitelisted: true
+    }
+  ));
+  
   await app.listen(PORT);
-
   console.log(`✅ Server running at http://${DOMAIN}:${PORT}/`);
 }
 bootstrap();
