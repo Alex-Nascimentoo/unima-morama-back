6a1e04c00101c1a1f30376a0136813a874148aa3
Guilherme D. Possas <possasg@gmail.com>
2024-05-07 14:47:10 -0300

    refact: change create client dto from camel case to snake case

--- src/client/client.controller.ts
+++ src/client/client.controller.ts
@@ -4,26 +4,27 @@ import { CreateClientDto } from './dto/create-client.dto';
 import { UpdateClientDto } from './dto/update-client.dto';
 
 @Controller('/client')
-export class ClientController {
-  constructor(private readonly clientService: ClientService) {}
+export class ClientController 
+{
+  constructor( private readonly client_service: ClientService ) {}
 
   @Post()
-  create(@Body() create_client_dto: CreateClientDto) {
-    return this.clientService.create(create_client_dto);
+  create( @Body() create_client_dto: CreateClientDto ) {
+    return this.client_service.create( create_client_dto );
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.clientService.findOne(+id);
+  @Get( ':id' )
+  findOne( @Param('id') id: number ) {
+    return this.client_service.findOne(+id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() update_client_dto: UpdateClientDto) {
-    return this.clientService.update(+id, update_client_dto);
+  update( @Param('id') id: number, @Body() update_client_dto: UpdateClientDto ) {
+    return this.client_service.update(+id, update_client_dto);
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.clientService.remove(+id);
+  @Delete( ':id' )
+  remove( @Param('id') id: number ) {
+    return this.client_service.remove(+id);
   }
 }
