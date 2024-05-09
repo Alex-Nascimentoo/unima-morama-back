12e3e913bc8af06d362f5c233d3467fd84b61357
Guilherme D. Possas <possasg@gmail.com>
2024-05-09 08:51:13 -0300

    feat: import authmodule and Config Module(enable get variables from .env)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
 import { ClientModule } from './client/client.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [ClientModule, PrismaModule]
+  imports: [
+    ClientModule, 
+    PrismaModule, 
+    AuthModule,
+    ConfigModule.forRoot( { isGlobal: true } )
+  ]
 })
 export class AppModule {}
