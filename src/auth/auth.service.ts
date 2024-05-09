7ca8fa056f411500ce99b2e7362bd49cb20cf0d0
Guilherme D. Possas <possasg@gmail.com>
2024-05-09 08:56:50 -0300

    feat: create sign in service

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,25 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { compare } from 'bcrypt';
+import { ClientService } from 'src/client/client.service';
+
+
+@Injectable()
+export class AuthService 
+{
+  constructor( private client_service: ClientService, private jwt_service: JwtService ) {}
+
+
+  async signIn( email: string, request_password: string )
+  {
+    const client = await this.client_service.get_by_email( email );
+
+    if ( compare( request_password, client.password ) ) 
+    {
+      throw new UnauthorizedException();
+    }
+
+    const payload = { sub: client.id, email: email }
+    return { access_token: await this.jwt_service.signAsync( payload ) };
+  }
+}
