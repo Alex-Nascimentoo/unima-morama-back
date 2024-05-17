48d0fedac6fc15f0b7f061aa3dcfb38a1a4978a2
Guilherme D. Possas <possasg@gmail.com>
2024-05-17 12:55:46 -0300

    feat: add supplier module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,16 @@ import { ClientModule } from './client/client.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
+import { SupplierController } from './supplier/supplier.controller';
+import { SupplierModule } from './supplier/supplier.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot( { isGlobal: true } ),
     ClientModule, 
     PrismaModule, 
     AuthModule,
-    ConfigModule.forRoot( { isGlobal: true } )
-  ]
+    SupplierModule
+  ],
 })
 export class AppModule {}
