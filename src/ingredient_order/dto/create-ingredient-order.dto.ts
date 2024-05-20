a50df5a2eae0667d911f075ba5dea6e37d634f80
Guilherme D. Possas <possasg@gmail.com>
2024-05-20 16:37:34 -0300

    feat: add ingredientOrder DTO

---
+++ src/ingredient_order/dto/create-ingredient-order.dto.ts
@@ -0,0 +1,20 @@
+import { UnitSystem } from "@prisma/client";
+import { IsEnum, IsInt, IsNumber } from "class-validator";
+
+export class CreateIngredientOrderDto
+{
+  @IsInt()
+  quantity: number
+
+  @IsNumber()
+  price: number
+
+  @IsEnum( UnitSystem )
+  unit_system: UnitSystem
+
+  @IsInt()
+  ingredient: number
+
+  @IsInt()
+  client: number
+}
