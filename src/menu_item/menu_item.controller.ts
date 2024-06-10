ef52994d534f1d3b46e44a8c9a40ab0b69dc9a7e
Alex Nacimento <alexdavi150@gmail.com>
2024-06-09 21:52:17 -0300

    chore: add route to get menu item by id

--- src/menu_item/menu_item.controller.ts
+++ src/menu_item/menu_item.controller.ts
@@ -1,31 +1,43 @@
-import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, Post, Query } from '@nestjs/common';
-import { MenuItemService } from './menu_item.service' 
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Post,
+} from '@nestjs/common';
+import { MenuItemService } from './menu_item.service';
 import { GetUser } from 'src/auth/decorators/get_user';
 import { CreateMenuItemDto } from './dto/create-menu-item.dto';
 
 @Controller('/menu-item')
 export class MenuItemController {
-	constructor( private readonly menu_item_service: MenuItemService ){}
+  constructor(private readonly menu_item_service: MenuItemService) {}
 
-	@HttpCode( HttpStatus.CREATED )
+  @HttpCode(HttpStatus.CREATED)
   @Post()
-  create( @GetUser() user_id: number, @Body() create_menu_item_dto: CreateMenuItemDto )
-  {
-    return this.menu_item_service.create( user_id, create_menu_item_dto );
+  create(
+    @GetUser() user_id: number,
+    @Body() create_menu_item_dto: CreateMenuItemDto,
+  ) {
+    return this.menu_item_service.create(user_id, create_menu_item_dto);
   }
 
-	@HttpCode( HttpStatus.NO_CONTENT )
-  @Delete( '/:id' )
-  delete( @GetUser() user_id: number, @Param( 'id' ) id: number )
-  {
-    return this.menu_item_service.delete_by_id( user_id, id );
-  }
-	
-	@Get()
-  get_client_menu_item( @GetUser() user_id: number )
-  {
-    return this.menu_item_service.find_all( user_id );
+  @HttpCode(HttpStatus.NO_CONTENT)
+  @Delete('/:id')
+  delete(@GetUser() user_id: number, @Param('id') id: number) {
+    return this.menu_item_service.delete_by_id(user_id, id);
   }
 
+  @Get()
+  get_client_menu_item(@GetUser() user_id: number) {
+    return this.menu_item_service.find_all(user_id);
+  }
 
+  @Get('/:id')
+  get_menu_item(@GetUser() user_id: number, @Param('id') id: number) {
+    return this.menu_item_service.find_by_id(user_id, id);
+  }
 }
